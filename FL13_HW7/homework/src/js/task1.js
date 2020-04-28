b7d1cacf11ae82fd90fa3baba59c4295defce242
Serhii Velianskyi <sergiy.velyanskiy48@gmail.com>
2020-04-28 21:29:18 +0300

    FL13_HW7

--- FL13_HW7/homework/src/js/task1.js
+++ FL13_HW7/homework/src/js/task1.js
@@ -1,8 +1,10 @@
+let tw = 20;
+let fr = 4;
 let login = prompt('Enter login:');
 if (login === 'User' || login === 'Admin') {
     let password = prompt('Enter password');
     if (login === 'User' && password === 'UserPass' || login === 'Admin' && password === 'RootPass') {
-        if (new Date().getHours() < 20) {
+        if (new Date().getHours() < tw) {
             if (login === 'User') {
                 alert('Good day, dear User!');
             } else {
@@ -22,7 +24,7 @@ if (login === 'User' || login === 'Admin') {
     }
 } else if (login === '' || login === null) {
     alert('Canceled.');
-} else if (login.length < 4) {
+} else if (login.length < fr) {
     alert('I don\'t know any users having name length less than 4 symbols')
 } else {
     alert('I don\'t know you');
--- FL13_HW7/homework/src/js/task2.js
+++ FL13_HW7/homework/src/js/task2.js
@@ -1,7 +1,43 @@
 let play = confirm('Do you want to play a game?');
 if (play) {
-    let num = Math.random(0, 5);
-    let att = 3;
+    let prize = 0;
+    let n = 1;
+    while (n !== 0) {
+        let f = 5;
+        let rand = f * n + 1;
+        let num = Math.floor(Math.random() * rand);
+        console.log(num);
+        let t = 3;
+        for (let i = 0; i < t; i++) {
+            let go = 1;
+            let tw = 2;
+            let fif = 50;
+            let popr = fif * Math.pow(tw, n - i);
+            let answer = +prompt(`Choose a roulette pocket number from 0 to ${f*n}\nAttempts left: ${t-i}
+Total prize: ${prize}$\nPossible prize on current attempt: ${popr}$`);
+            if (answer === num) {
+                prize = prize + popr;
+                go = confirm(`Congratulation, you won!\nYour prize is: ${prize}$.\nDo you want to continue?`);
+                if (go) {
+                    n++;
+                    break;
+                } else {
+                    n = 0;
+                }
+            }
+            if (i === tw && answer !== num || !go) {
+                alert(`Thank you for your participation.\nYour prize is: ${prize}$`)
+                go = confirm(`Do you want to play again?`);
+                if (go) {
+                    n = 1;
+                    prize = 0;
+                } else {
+                    n = 0;
+                }
+                break;
+            }
+        }
+    }
 } else {
     alert('You did not become a billionaire, but can.');
 }
